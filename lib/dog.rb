aa1dafe399435db9b8a3343ed20c2466491612f5
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-08 01:58:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,8 +24,19 @@ class Dog
     end
     
     def save 
+      if
+        self.id == true
+        update
+        
+      else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?,?);
       
-      
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB{:conn}.execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      end
+    self
     end
   
 end
